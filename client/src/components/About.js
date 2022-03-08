252c0c492616cab71f8b86202771dce68edbf6fc
Defne Celikoyar <defne.celikoyar@gmail.com>
2022-03-08 16:59:11 -0500

    initial commit

---
+++ client/src/components/About.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function About() {
+  return (
+    <div style={{color: "#8B0000"}} className="container">hi I'm defne and this is my personal website</div>
+  );
+}
+
+export default About;
